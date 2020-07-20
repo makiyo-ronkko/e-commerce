56de588ee72b923188b086a2c93e662fba12ce72
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-07-20 17:19:46 +0300

    Setup body-parser

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// urlencoded to handle html form
+// Automatically detect if bodyParse requred and apply
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // root router
 app.get('/', (req, res) => {
@@ -17,28 +22,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-
-// bodyParser is a middleware function
-// next is a callback function which is app.post('/', bodyParser, (req, res) => {}
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', data => {//on = addEventListener 'data' event
-            // return <Buffer> is an array contains raw info
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    }
-};
-
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
     //get access to email, password, passwordConfirmation
     console.log(req.body);
     res.send('Account created');
@@ -67,3 +51,23 @@ Without Express Middleware
     });
     res.send('Account created');
 }); */
+
+// bodyParser is a middleware function
+// next is a callback function which is app.post('/', bodyParser, (req, res) => {}
+/* const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') {
+        req.on('data', data => {//on = addEventListener 'data' event
+            // return <Buffer> is an array contains raw info
+            const parsed = data.toString('utf8').split('&');
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            req.body = formData;
+            next();
+        });
+    } else {
+        next();
+    }
+}; */
