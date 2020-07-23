b73b03b949045dc13c129ce10fae4f178e18ce0a
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-07-23 10:50:20 +0300

    Setup signin user logic

--- index.js
+++ index.js
@@ -76,7 +76,18 @@ app.get('/signin', (req, res) => {
 });
 
 app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email });
 
+    if (!user) {
+        return res.send('Email not found');
+    }
+    if (user.password !== password) {//password from input
+        return res.send('Invalid password');
+    }
+    // Store the id of that user inside the users cookie
+    req.session.userId = user.id;
+    res.send('You are signed in');
 });
 
 app.listen(3000, () => {
