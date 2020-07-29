d0d69b15bd25a96462b8878afaa269f97bb373a1
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-07-29 16:18:13 +0300

    Install multer to upload image datafile

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 // list out products via administrator view
 router.get('/admin/products', (req, res) => {
@@ -19,10 +22,14 @@ router.get('/admin/products/new', (req, res) => {
 });
 
 // product submission
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'), (req, res) => {
     // express-validator error check
     const errors = validationResult(req);
-    console.log(errors);
+    //console.log(errors);
+    //console.log(req.body);
+    // 
+
+    console.log(req.file);//fieldname, originalname, encoding, mimetype, buffer, size
     res.send('submitted');
 });
 
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,7 +4,7 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
     return layout({
         content: `
-        <form method="POST">
+        <form method="POST" enctype="multipart/form-data">
             <input placeholder="Title" name="title" />
             <input placeholder="Price" name="price" />
             <input type="file" name="image" />
@@ -13,3 +13,8 @@ module.exports = ({ errors }) => {
 `
     });
 };
+
+// default method is "GET"
+// enctype = encoding type
+// default enctype="application/x-www-form-urlencoded"
+// data is transmitted safely inside of url
