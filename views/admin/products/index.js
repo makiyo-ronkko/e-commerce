afa874cf5be3ffe00cfcb2611773a5519fb6a08c
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-07-30 12:52:17 +0300

    Render product list

---
+++ views/admin/products/index.js
@@ -0,0 +1,19 @@
+const layout = require('../layout');
+
+module.exports = ({ products }) => {
+    const renderedProducts = products
+        .map(product => {
+            return `
+      <div>${product.title}</div>
+      <div>${product.price}</div>
+    `;
+        })
+        .join('');
+
+    return layout({
+        content: `
+      <h1 class="title">Products</h1>
+      ${renderedProducts}
+    `
+    });
+};
